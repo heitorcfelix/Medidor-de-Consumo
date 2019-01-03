c2a5c0c52b6ebde8b1fc77980e31664b9d0aa09c
Heitor Felix <heitorcfelix@gmail.com>
2019-01-03 18:17:38 -0300

    refreshing values on database change

---
+++ gulp-tasks/serve.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var browserSync = require('browser-sync').create(),
+  gulp = require('gulp'),
+  nodemon = require('gulp-nodemon'),
+  reload = browserSync.reload;
+
+gulp.task('browser-sync',
+  [
+    'nodemon'
+  ],
+  function() {
+    browserSync.init(null, {
+      proxy: 'http://localhost:8080',
+      browser: 'google-chrome',
+      port: 5000
+    });
+  }
+);
+
+gulp.task('nodemon',
+  [],
+  function (done) {
+    var running = false;
+
+    return nodemon({
+      script: 'medidor.js',
+      watch: ['database.db']
+    })
+    .on('start', function () {
+      if (!running) {
+        done();
+      }
+      running = true;
+    })
+    .on('restart', function () {
+      setTimeout(function () {
+        reload();
+      }, 500);
+    });
+  }
+);
